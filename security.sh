07ec8366fe46f5850deb2b8084797eab7e51ee34
rmkn <bubblun@gmail.com>
2016-11-25 13:38:40 +0900

    change config

--- security.sh
+++ security.sh
@@ -2,4 +2,8 @@
 
 cat <<EOT > /etc/nginx/conf.d/security.conf 
 server_tokens off;
+add_header X-XSS-Protection "1; mode=block";
+add_header X-Frame-Options DENY;
+add_header X-Content-Type-Options nosniff;
+add_header Content-Security-Policy "default-src 'self'";
 EOT
